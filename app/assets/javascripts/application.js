389afa1a66c864cfabf6a080b2c16a3b208732bc
Cameron Banowsky <cameron@hack.cd>
2016-10-13 05:43:41 -0700

    js addition

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-= require jquery
-= require jquery_ujs
-= require_tree .
-= require scrivito
-= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+//= require scrivito
+//= require bootstrap-sprockets
 
